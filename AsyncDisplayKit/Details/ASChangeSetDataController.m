69f1946fd0befbaec16ee1d0438eed3923a40fe4
Adlai Holler <adlai@pinterest.com>
2016-06-30 13:17:29 -0700

    [ASDataController] Add validation logic to the change set to throw exceptions on invalid updates

--- AsyncDisplayKit/Details/ASChangeSetDataController.m
+++ AsyncDisplayKit/Details/ASChangeSetDataController.m
@@ -30,8 +30,7 @@
   // NOTE: This assertion is failing in some apps and will be enabled soon.
 //  ASDisplayNodeAssertMainThread();
   if (_changeSetBatchUpdateCounter <= 0) {
-    _changeSet = [_ASHierarchyChangeSet new];
-    _changeSetBatchUpdateCounter = 0;
+    _changeSet = [[_ASHierarchyChangeSet alloc] initWithOldData:[self _itemCountsFromDataSource]];
   }
   _changeSetBatchUpdateCounter++;
 }
@@ -47,7 +46,7 @@
 //  NSAssert(_changeSetBatchUpdateCounter >= 0, @"endUpdatesAnimated:completion: called without having a balanced beginUpdates call");
   
   if (_changeSetBatchUpdateCounter == 0) {
-    [_changeSet markCompleted];
+    [_changeSet markCompletedWithNewItemCounts:[self _itemCountsFromDataSource]];
     
     [super beginUpdates];
 
@@ -174,6 +173,17 @@
   } else {
     [super moveRowAtIndexPath:indexPath toIndexPath:newIndexPath withAnimationOptions:animationOptions];
   }
+}
+
+- (NSArray <NSNumber *> *)_itemCountsFromDataSource
+{
+  id<ASDataControllerSource> source = self.dataSource;
+  NSInteger sectionCount = [source numberOfSectionsInDataController:self];
+  NSMutableArray<NSNumber *> *result = [NSMutableArray arrayWithCapacity:sectionCount];
+  for (NSInteger i = 0; i < sectionCount; i++) {
+    [result addObject:@([source dataController:self rowsInSection:i])];
+  }
+  return result;
 }
 
 @end
